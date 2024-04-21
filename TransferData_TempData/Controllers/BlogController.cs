4a1be28c9471fcf74c1d55764a7c186fc3e4d272
Mohammadreza <ahadian2@gmail.com>
2024-04-21 17:14:00 +0330

    Add project files.

---
+++ TransferData_TempData/Controllers/BlogController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TransferData_TempData.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            var Name = TempData["Name"];
+            return View();
+        }
+    }
+}
